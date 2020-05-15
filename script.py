aa1d3b02cfe04a0fa83859b62c97dadc6b1308f3
Yafet Sutanto <ysutanto22@Gmail.com>
2020-05-15 13:02:50 +0700

    Create script.py

---
+++ script.py
@@ -0,0 +1,177 @@
+# %% [code]
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# You can write up to 5GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+
+# %% [code]
+import pandas as pd
+import numpy as np
+import pandas as pd
+import seaborn as sns
+sns.set_palette('husl')
+import matplotlib.pyplot as plt
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelBinarizer
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+# %% [code]
+animeCleanedFilePath = "../input/myanimelist/anime_cleaned.csv"
+data = pd.read_csv(animeCleanedFilePath)
+data.head()
+
+# %% [code]
+data['studio'].count()
+
+# %% [code]
+data['studio'].value_counts(dropna=True)
+
+# %% [code]
+processedData = data.loc[data['studio'].isin(['Toei Animation','Sunrise','Madhouse','Studio Pierrot','J.C.Staff'])]
+
+# %% [code]
+newDf = pd.DataFrame({'studio': processedData['studio'], 'score': processedData['score']}, columns = ['studio','score'])
+X = newDf.drop(['studio'], axis = 1)
+y = newDf['studio']
+
+# print(X.head())
+print(X.shape)
+# print(y.head())
+print(y.shape)
+
+# %% [code]
+k_range = list(range(1,50))
+scores = []
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X, y)
+    y_pred = knn.predict(X)
+    scores.append(metrics.accuracy_score(y, y_pred))
+    
+plt.plot(k_range, scores)
+plt.xlabel('Value of k for KNN')
+plt.ylabel('Accuracy Score')
+plt.title('Accuracy Scores for Values of k of k-Nearest-Neighbors')
+plt.show()
+
+# %% [code]
+logreg = LogisticRegression()
+logreg.fit(X, y)
+y_pred = logreg.predict(X)
+print(metrics.accuracy_score(y, y_pred))
+
+# %% [code]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=5)
+print(X_train.shape)
+print(y_train.shape)
+print(X_test.shape)
+print(y_test.shape)
+
+# %% [code]
+k_range = list(range(1,50))
+scores = []
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    scores.append(metrics.accuracy_score(y_test, y_pred))
+    
+plt.plot(k_range, scores)
+plt.xlabel('Value of k for KNN')
+plt.ylabel('Accuracy Score')
+plt.title('Accuracy Scores for Values of k of k-Nearest-Neighbors')
+plt.show()
+
+# %% [code]
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+y_pred = logreg.predict(X_test)
+print(metrics.accuracy_score(y_test, y_pred))
+
+# %% [code]
+knn = KNeighborsClassifier(n_neighbors=12)
+knn.fit(X, y)
+studio_animes_rating = 3
+knn.predict([[studio_animes_rating]])
+
+# %% [code]
+newDf = pd.DataFrame({'studio': data['studio'], 'score': data['score']}, columns = ['studio','score'])
+X = newDf.drop(['studio'], axis = 1)
+y = newDf['studio']
+
+# print(X.head())
+print(X.shape)
+# print(y.head())
+print(y.shape)
+
+# %% [code]
+k_range = list(range(1,50))
+scores = []
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X, y)
+    y_pred = knn.predict(X)
+    scores.append(metrics.accuracy_score(y, y_pred))
+    
+plt.plot(k_range, scores)
+plt.xlabel('Value of k for KNN')
+plt.ylabel('Accuracy Score')
+plt.title('Accuracy Scores for Values of k of k-Nearest-Neighbors')
+plt.show()
+
+# %% [code]
+logreg = LogisticRegression()
+logreg.fit(X, y)
+y_pred = logreg.predict(X)
+print(metrics.accuracy_score(y, y_pred))
+
+# %% [code]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=5)
+print(X_train.shape)
+print(y_train.shape)
+print(X_test.shape)
+print(y_test.shape)
+
+# %% [code]
+k_range = list(range(1,50))
+scores = []
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    scores.append(metrics.accuracy_score(y_test, y_pred))
+    
+plt.plot(k_range, scores)
+plt.xlabel('Value of k for KNN')
+plt.ylabel('Accuracy Score')
+plt.title('Accuracy Scores for Values of k of k-Nearest-Neighbors')
+plt.show()
+
+# %% [code]
+logreg = LogisticRegression(max_iter=2000)
+logreg.fit(X_train, y_train)
+y_pred = logreg.predict(X_test)
+print(metrics.accuracy_score(y_test, y_pred))
+
+# %% [code]
+knn = KNeighborsClassifier(n_neighbors=12)
+knn.fit(X, y)
+studio_animes_rating = 8.71
+result_prediction = knn.predict([[studio_animes_rating]])
+print(result_prediction)
